2074f22aba2002083696f21bf057fb29466b0bdf
ymohit1603 <mohityadav0330@gmail.com>
2025-04-13 15:19:44 +0530

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -37,53 +37,53 @@ export function activate(ctx: vscode.ExtensionContext) {
     }),
   );
 
-  ctx.subscriptions.push(
-    vscode.commands.registerCommand(COMMAND_PROXY, function () {
-      wakatime.promptForProxy();
-    }),
-  );
+  // ctx.subscriptions.push(
+  //   vscode.commands.registerCommand(COMMAND_PROXY, function () {
+  //     wakatime.promptForProxy();
+  //   }),
+  // );
 
-  ctx.subscriptions.push(
-    vscode.commands.registerCommand(COMMAND_DEBUG, function () {
-      wakatime.promptForDebug();
-    }),
-  );
+  // ctx.subscriptions.push(
+  //   vscode.commands.registerCommand(COMMAND_DEBUG, function () {
+  //     wakatime.promptForDebug();
+  //   }),
+  // );
 
-  ctx.subscriptions.push(
-    vscode.commands.registerCommand(COMMAND_DISABLE, function () {
-      wakatime.promptToDisable();
-    }),
-  );
+  // ctx.subscriptions.push(
+  //   vscode.commands.registerCommand(COMMAND_DISABLE, function () {
+  //     wakatime.promptToDisable();
+  //   }),
+  // );
 
-  ctx.subscriptions.push(
-    vscode.commands.registerCommand(COMMAND_STATUS_BAR_ENABLED, function () {
-      wakatime.promptStatusBarIcon();
-    }),
-  );
+  // ctx.subscriptions.push(
+  //   vscode.commands.registerCommand(COMMAND_STATUS_BAR_ENABLED, function () {
+  //     wakatime.promptStatusBarIcon();
+  //   }),
+  // );
 
-  ctx.subscriptions.push(
-    vscode.commands.registerCommand(COMMAND_STATUS_BAR_CODING_ACTIVITY, function () {
-      wakatime.promptStatusBarCodingActivity();
-    }),
-  );
+  // ctx.subscriptions.push(
+  //   vscode.commands.registerCommand(COMMAND_STATUS_BAR_CODING_ACTIVITY, function () {
+  //     wakatime.promptStatusBarCodingActivity();
+  //   }),
+  // );
 
-  ctx.subscriptions.push(
-    vscode.commands.registerCommand(COMMAND_DASHBOARD, function () {
-      wakatime.openDashboardWebsite();
-    }),
-  );
+  // ctx.subscriptions.push(
+  //   vscode.commands.registerCommand(COMMAND_DASHBOARD, function () {
+  //     wakatime.openDashboardWebsite();
+  //   }),
+  // );
 
-  ctx.subscriptions.push(
-    vscode.commands.registerCommand(COMMAND_CONFIG_FILE, function () {
-      wakatime.openConfigFile();
-    }),
-  );
+  // ctx.subscriptions.push(
+  //   vscode.commands.registerCommand(COMMAND_CONFIG_FILE, function () {
+  //     wakatime.openConfigFile();
+  //   }),
+  // );
 
-  ctx.subscriptions.push(
-    vscode.commands.registerCommand(COMMAND_LOG_FILE, function () {
-      wakatime.openLogFile();
-    }),
-  );
+  // ctx.subscriptions.push(
+  //   vscode.commands.registerCommand(COMMAND_LOG_FILE, function () {
+  //     wakatime.openLogFile();
+  //   }),
+  // );
 
   ctx.subscriptions.push(wakatime);
 
